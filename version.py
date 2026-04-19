1b6add4b9d4a32e3e63e955ba80b683705576ffc
ahmedomeer <91143386+ahmedomeer@users.noreply.github.com>
2026-04-19 03:09:05 +0200

    Delete version.py

--- version.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-# This file previously monkey-patched the server version to mark the edition as
-# enterprise and updated RPC metadata. This behavior has been intentionally
-# disabled to fully remove subscription/licensing markers.
-# Import remains here to keep file import-safe and explicit.
-import odoo  # noqa: F401
